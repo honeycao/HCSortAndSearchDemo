51e6b0d69c7ce2e6b0e3d46a6069be00727d5ef6
honeycao <a51@51s-mac-mini.local>
2016-03-28 17:00:58 +0800

    Initial Commit

---
+++ FriendSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FriendSearch
+//
+//  Created by Caoyq on 16/3/28.
+//  Copyright © 2016年 Caoyq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
